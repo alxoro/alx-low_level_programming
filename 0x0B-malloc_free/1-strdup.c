31cabe33d1deffbf4078743859e0274e63e27395
alxoro <amonkatam7@gmail.com>
2023-07-10 14:10:37 -0700

    main.h

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - it copy
+ * @str: poiter to string
+ * Return: pinter to a new string
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0, k;
+
+	if (!str)
+		return (NULL);
+	while (*(str + i))
+		i++;
+
+	s = malloc(sizeof(char) * i);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+	{
+		s[k] = str[k];
+	}
+	return (s);
+}
