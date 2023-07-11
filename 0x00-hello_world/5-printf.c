6e58242ccc26e7cf21ce9678993960d40ec5c883
alxoro <amonkatam7@gmail.com>
2023-07-11 09:05:57 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+/**
+ *main - This funtion prints a sentence followed by new line
+ *Description: This funtion prints a sentence followed by new line
+ *@void: return type
+ *Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
