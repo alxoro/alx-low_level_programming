e62e81fd19a78a1aecb43d6d2af43fa21264052e
alxoro <amonkatam7@gmail.com>
2023-07-28 17:21:49 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void first(void)__attribute__((constructor));
+/**
+ * first - it is printing a sentence before the main
+ * the functionn is executed
+ */
+void firts(void)
+{
+	printf("give me that! or else, we will be at loggerhead,\n");
+	printf("i am the skulpture of myself!\n");
+}
