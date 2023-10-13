5b62d6580f12f30b3ec13ef2d7177f90901f9e7f
alxoro <amonkatam7@gmail.com>
2023-10-13 18:04:21 +0300

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns NULL node of a dlistint_t linked list.
+ * @head: list head pointer
+ * @index: index of the node to search for starting from 0
+ * Return: NULL
+ **/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int size;
+	dlistint_t *tmp;
+
+	size = 0;
+	if (head == NULL)
+	return (NULL);
+	tmp = head;
+	while (tmp)
+	{
+	if (index == size)
+	return (tmp);
+	size++;
+	tmp = tmp->next;
+	}
+	return (NULL);
+}
